82c0356784dc0234926233924098e77ca94bf3f6
Claire Li <s3310507057@gmail.com>
2022-08-24 23:21:42 +0800

    feat: project init

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/airbnb',
+  ],
+
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+
+  rules: {
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
